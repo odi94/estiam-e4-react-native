962a990a202aebb4fd8b7c58ba962f7579005628
Personnel <superabel000@gmail.com>
2026-03-17 14:43:48 +0100

    refactor: optimize IconSymbol for universal platform support

--- foodie-spot/components/ui/icon-symbol.tsx
+++ foodie-spot/components/ui/icon-symbol.tsx
@@ -20,6 +20,7 @@ const MAPPING = {
   'chevron.right': 'chevron-right',
   'magnifyingglass': 'search',
   'person.fill': 'person',
+  'alarm.fill': 'notifications',
   'bag': 'shopping-bag',
 } as IconMapping;
 
